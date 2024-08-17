71e81a68a41a049e7fbbd9be1f5db9352a28b8c9
Sivanush <sivanushedapal@gmail.com>
2024-08-17 14:45:34 +0530

    fixing the names in env file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,25 +1,25 @@
 export const environment = {
     production: true,
-    apiUrl: process.env["API_URL"],
-    domain: process.env["DOMAIN"],
-    googleClientId: process.env["GOOGLE_CLIENT_ID"],
+    apiUrl: process.env["apiUrl"],
+    domain: process.env["domain"],
+    googleClientId: process.env["googleClientId"],
   };
   
   export const firebaseConfig = {
-    apiKey: process.env["FIREBASE_API_KEY"],
-    authDomain: process.env["FIREBASE_AUTH_DOMAIN"],
-    databaseURL: process.env["FIREBASE_DATABASE_URL"],
-    projectId: process.env["FIREBASE_PROJECT_ID"],
-    storageBucket: process.env["FIREBASE_STORAGE_BUCKET"],
-    messagingSenderId: process.env["FIREBASE_MESSAGING_SENDER_ID"],
-    appId: process.env["FIREBASE_APP_ID"],
-    measurementId: process.env["FIREBASE_MEASUREMENT_ID"],
+    apiKey: process.env["apiKey"],
+    authDomain: process.env["authDomain"],
+    databaseURL: process.env["databaseURL"],
+    projectId: process.env["projectId"],
+    storageBucket: process.env["storageBucket"],
+    messagingSenderId: process.env["messagingSenderId"],
+    appId: process.env["appId"],
+    measurementId: process.env["measurementId"],
   };
   
-  export const socketUrl = process.env["SOCKET_URL"];
+  export const socketUrl = process.env["socketUrl"];
   
   export const awsCredentials = {
-    accessKey: process.env["AWS_ACCESS_KEY"],
-    secretKey: process.env["AWS_SECRET_KEY"],
+    accessKey: process.env["Access_key"],
+    secretKey: process.env["Secret_key"],
   };
   
