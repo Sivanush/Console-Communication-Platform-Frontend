2cc1630a956ed64a99962cbc82a6695c2e95a566
Sivanush <sivanushedapal@gmail.com>
2024-08-17 14:56:13 +0530

    changes in the env structure

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,25 +1,49 @@
+// export const environment = {
+//     production: true,
+//     apiUrl: process.env["apiUrl"],
+//     domain: process.env["domain"],
+//     googleClientId: process.env["googleClientId"],
+//   };
+  
+//   export const firebaseConfig = {
+//     apiKey: process.env["apiKey"],
+//     authDomain: process.env["authDomain"],
+//     databaseURL: process.env["databaseURL"],
+//     projectId: process.env["projectId"],
+//     storageBucket: process.env["storageBucket"],
+//     messagingSenderId: process.env["messagingSenderId"],
+//     appId: process.env["appId"],
+//     measurementId: process.env["measurementId"],
+//   };
+  
+//   export const socketUrl = process.env["socketUrl"];
+  
+//   export const awsCredentials = {
+//     accessKey: process.env["Access_key"],
+//     secretKey: process.env["Secret_key"],
+//   };
+  
+
+
 export const environment = {
     production: true,
     apiUrl: process.env["apiUrl"],
     domain: process.env["domain"],
     googleClientId: process.env["googleClientId"],
-  };
-  
-  export const firebaseConfig = {
-    apiKey: process.env["apiKey"],
-    authDomain: process.env["authDomain"],
-    databaseURL: process.env["databaseURL"],
-    projectId: process.env["projectId"],
-    storageBucket: process.env["storageBucket"],
-    messagingSenderId: process.env["messagingSenderId"],
-    appId: process.env["appId"],
-    measurementId: process.env["measurementId"],
-  };
-  
-  export const socketUrl = process.env["socketUrl"];
-  
-  export const awsCredentials = {
-    accessKey: process.env["Access_key"],
-    secretKey: process.env["Secret_key"],
+    firebaseConfig: {
+      apiKey: process.env["apiKey"],
+      authDomain: process.env["authDomain"],
+      databaseURL: process.env["databaseURL"],
+      projectId: process.env["projectId"],
+      storageBucket: process.env["storageBucket"],
+      messagingSenderId: process.env["messagingSenderId"],
+      appId: process.env["appId"],
+      measurementId: process.env["measurementId"],
+    },
+    socketUrl: process.env["socketUrl"],
+    awsCredentials: {
+      accessKey: process.env["Access_key"],
+      secretKey: process.env["Secret_key"],
+    },
   };
   
