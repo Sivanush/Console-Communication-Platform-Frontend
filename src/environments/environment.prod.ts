3259502739ff0043130ff374dd6151c994e5ef7f
Sivanush <sivanushedapal@gmail.com>
2024-08-17 15:32:02 +0530

    added types file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -51,9 +51,37 @@
 
 
 
+// export const environment = {
+//     production: true,
+//     apiUrl: process.env["apiUrl"] || 'defaultApiUrl', // Provide a default value if needed
+//     domain: process.env["domain"] || 'defaultDomain',
+//     googleClientId: process.env["googleClientId"] || 'defaultGoogleClientId',
+//   };
+  
+//   export const firebaseConfig = {
+//     apiKey: process.env["apiKey"] || 'defaultApiKey',
+//     authDomain: process.env["authDomain"] || 'defaultAuthDomain',
+//     databaseURL: process.env["databaseURL"] || 'defaultDatabaseURL',
+//     projectId: process.env["projectId"] || 'defaultProjectId',
+//     storageBucket: process.env["storageBucket"] || 'defaultStorageBucket',
+//     messagingSenderId: process.env["messagingSenderId"] || 'defaultMessagingSenderId',
+//     appId: process.env["appId"] || 'defaultAppId',
+//     measurementId: process.env["measurementId"] || 'defaultMeasurementId',
+//   };
+  
+//   export const socketUrl = process.env["socketUrl"] || 'defaultSocketUrl';
+  
+//   export const awsCredentials = {
+//     accessKey: process.env["accessKey"] || 'defaultAccessKey',
+//     secretKey: process.env["secretKey"] || 'defaultSecretKey',
+//   };
+  
+
+
+
 export const environment = {
     production: true,
-    apiUrl: process.env["apiUrl"] || 'defaultApiUrl', // Provide a default value if needed
+    apiUrl: process.env["apiUrl"] || 'defaultApiUrl',
     domain: process.env["domain"] || 'defaultDomain',
     googleClientId: process.env["googleClientId"] || 'defaultGoogleClientId',
   };
@@ -75,4 +103,3 @@ export const environment = {
     accessKey: process.env["accessKey"] || 'defaultAccessKey',
     secretKey: process.env["secretKey"] || 'defaultSecretKey',
   };
-  
