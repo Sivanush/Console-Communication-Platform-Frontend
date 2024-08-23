6307fa76fb0cda96df87b777084aa82b86888397
Sivanush <sivanushedapal@gmail.com>
2024-08-23 19:21:38 +0530

    trying TURN Server

--- src/app/service/server-voice-call/server-voice-call.service.ts
+++ src/app/service/server-voice-call/server-voice-call.service.ts
@@ -398,10 +398,28 @@ export class ServerVoiceCallService {
   
     this.peer = new Peer(peerId, {
       debug: 3,
-      config: {
+      config: { 
         iceServers: [
           { urls: 'stun:stun.l.google.com:19302' },
           { urls: 'stun:stun1.l.google.com:19302' },
+          {
+            "urls": "stun:global.stun.twilio.com:3478"
+          },
+          {
+            "username": "dc2d2894d5a9023620c467b0e71cfa6a35457e6679785ed6ae9856fe5bdfa269",
+            "credential": "tE2DajzSJwnsSbc123",
+            "urls": "turn:global.turn.twilio.com:3478?transport=udp"
+          },
+          {
+            "username": "dc2d2894d5a9023620c467b0e71cfa6a35457e6679785ed6ae9856fe5bdfa269",
+            "credential": "tE2DajzSJwnsSbc123",
+            "urls": "turn:global.turn.twilio.com:3478?transport=tcp"
+          },
+          {
+            "username": "dc2d2894d5a9023620c467b0e71cfa6a35457e6679785ed6ae9856fe5bdfa269",
+            "credential": "tE2DajzSJwnsSbc123",
+            "urls": "turn:global.turn.twilio.com:443?transport=tcp"
+          }
         ]
       }
     });
